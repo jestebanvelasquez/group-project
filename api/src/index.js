76db7ba77f52aa26c051594e2a96d772f880326d
Esteban Velasquez <jestebanvelasquez22@gmail.com>
2022-08-02 21:35:19 -0500

    provando modelos

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,13 @@
-hello
+const express = require('express');
+const app = express();
+
+//midlewares:
+app.use(express.json())
+app.get('/', (req, res) =>{
+    console.log('hola');
+    res.status(200).json({data:'hola!'})
+})
+
+app.listen(4000, () => {
+    console.log('artistApp')
+})
