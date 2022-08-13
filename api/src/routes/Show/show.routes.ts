600b4dde495946f8eb495f86c7175d94528bd6cd
Maikyls <85658257+Maikyls@users.noreply.github.com>
2022-08-13 18:14:45 -0500

    Agrego ruta para encontrar eventos por nombre

--- api/src/routes/Show/show.routes.ts
+++ api/src/routes/Show/show.routes.ts
@@ -5,7 +5,8 @@ import showController from "../../controllers/show.controller";
 
 
 //http://localhost:4000/...
-router.get('/shows', showController.getShows);
+router.get('/shows', showController.getShowsByArtist);
+router.get('/shows/eventName', showController.getShowsByArtist);
 router.get('/shows/:id', showController.getShowsById);
 router.post('/createShow/:id', showController.createShow);
 
