c93544279c8ec5dd97c95987710fe435afb4c93a
Juan Sebastian Artunduaga Rojas <jartunduaga7896@gmail.com>
2022-08-12 20:30:59 -0500

    Se crea la ruta de obtener show por id y por nickName

--- api/src/routes/Show/show.routes.ts
+++ api/src/routes/Show/show.routes.ts
@@ -4,14 +4,10 @@ const router = Router();
 import showController from "../../controllers/show.controller";
 
 
-
-
 //http://localhost:4000/...
-router.get('/shows', showController.getShows)
-router.post('/createShow/:id', showController.createShow)
-// router.get('/shows/name')
-// router.get('/shows/:id')
-
+router.get('/shows', showController.getShows);
+router.get('/shows/:id', showController.getShowsById);
+router.post('/createShow/:id', showController.createShow);
 
 
 export default router;
